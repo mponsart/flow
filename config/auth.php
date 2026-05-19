da8c62bc3ea387730e3169570b9a4ca54d1cf070
mponsart <maxime.ponsart@speed-cloud.fr>
2026-05-19 21:34:39 +0200

    Remove tiers and tiers_detail views along with their associated logic and scripts

--- config/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('GOOGLE_AUTH_URL',  'https://accounts.google.com/o/oauth2/v2/auth');
-define('GOOGLE_TOKEN_URL', 'https://oauth2.googleapis.com/token');
-define('GOOGLE_USERINFO_URL', 'https://www.googleapis.com/oauth2/v3/userinfo');
-define('GOOGLE_REDIRECT_URI', APP_URL . '/auth/google/callback');
-define('GOOGLE_SCOPE', 'openid email profile');
-define('ALLOWED_DOMAIN', getenv('ALLOWED_DOMAIN') ?: 'groupe-speed.cloud');
--- database/migrations/002_remove_dolibarr.sql
+++
@@ -1,8 +0,0 @@
--- Migration 002 : suppression des colonnes Dolibarr
--- À exécuter une seule fois après la mise en prod du refactoring
-
-ALTER TABLE tiers    DROP COLUMN IF EXISTS dolibarr_id;
-ALTER TABLE invoices DROP COLUMN IF EXISTS dolibarr_id;
-ALTER TABLE payments DROP COLUMN IF EXISTS dolibarr_id;
-ALTER TABLE products DROP COLUMN IF EXISTS dolibarr_id;
-DROP TABLE  IF EXISTS sync_logs;
